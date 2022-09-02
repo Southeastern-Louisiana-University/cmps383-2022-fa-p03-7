ade928c9cd178c1dbfb17418113ee1042c99333a
oretha francis <francis.oretha@selu.edu>
2022-09-01 21:46:36 -0500

    sql

--- FA22.P03.Web/Program.cs
+++ FA22.P03.Web/Program.cs
@@ -1,6 +1,9 @@
 using FA22.P03.Web.Features.Products;
 
+
 var builder = WebApplication.CreateBuilder(args);
+
+
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
