92aa7febfb389ce85d0d568ea621df1b0139bba7
dotbehrens <dotbehrens@gmail.com>
2022-09-03 17:44:00 -0500

    add entities and dtos

---
+++ FA22.P03.Web/Features/Entites/ItemListingEntity.cs
@@ -0,0 +1,13 @@
+namespace FA22.P02.Web.Features
+{
+
+
+
+
+    public class ItemListing
+    {
+        public Item Item { get; set; }
+        public Listing Listing { get; set; }
+
+    }
+}
---
+++ FA22.P03.Web/Features/Models/ListingDto.cs
@@ -0,0 +1,11 @@
+namespace FA22.P03.Web.Features.Products;
+
+
+
+public class ListingDto
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+}
