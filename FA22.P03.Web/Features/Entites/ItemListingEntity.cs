701459f9f3996303508199bc66371891cfa5057b
dotbehrens <dotbehrens@gmail.com>
2022-09-05 18:32:23 -0500

    Add Id property to ItemListing Entity

--- FA22.P03.Web/Features/Entites/ItemListingEntity.cs
+++ FA22.P03.Web/Features/Entites/ItemListingEntity.cs
@@ -6,6 +6,7 @@ namespace FA22.P02.Web.Features
 
     public class ItemListing
     {
+        public int Id { get; set; }
         public Item Item { get; set; }
         public Listing Listing { get; set; }
 
