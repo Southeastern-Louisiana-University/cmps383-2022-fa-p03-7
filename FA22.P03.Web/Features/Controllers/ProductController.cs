9423ea90ebb72c30ce6222697993683abdcd329e
dotbehrens <dotbehrens@gmail.com>
2022-09-05 20:41:37 -0500

    add GetAllProducts and GetProductsById

--- FA22.P03.Web/Features/Controllers/ProductController.cs
+++ FA22.P03.Web/Features/Controllers/ProductController.cs
@@ -1,23 +1,56 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Encodings.Web;
+// using System.Web.Http;
+using FA22.P03.Web.Features.Products;
+using FA22.P02.Web.Features;
+using Database;
+using System.Linq;
 
-
+[ApiController]
+[Route("[controller]")]
 public class ProductController : Controller
 {
-    // 
-    // GET: /HelloWorld/
+    private readonly DataContext _dataContext;
 
-    public string Index()
+
+    public ProductController(DataContext dataContext)
     {
-        return "This is my default action...";
+        _dataContext = dataContext;
     }
 
-    // 
-    // GET: /HelloWorld/Welcome/ 
+    //Todo GET /api/products
+    // returns list of all products 200
 
-    public string GetAllProducts()
+    [HttpGet("/api/products/")]
+    public IActionResult GetAllProducts()
     {
-        return "This is the Welcome action method...";
+        IList<ProductDto> products = _dataContext.Products.Distinct().Select<Product, ProductDto>((product) => new ProductDto { Id = product.Id, Name = product.Name, Description = product.Description }).ToList();
+        return Ok(products);
     }
+
+
+    //Todo GET /api/products/{id}/listings
+    // returns all current listings for the given product id 200
+
+    [HttpGet("/api/products/{id}")]
+    public IActionResult GetProductById(int id)
+    {
+
+        var result = _dataContext.Products.FirstOrDefault(x => x.Id == id);
+        if (result == null)
+        {
+            return NotFound();
+        }
+        return Ok(result);
+
+    }
+
+    //Todo Get /api/products/{id}
+    // find product by its unique id and return the details 200/404
+    //Todo Post /api/products
+    // name must be provided, no longer than 120 / must have description/ return created dto and location
+    //Todo PUT /api/products/{id}
+    // must have name 120 char max and description  return updated dto
+    //Todo DELETE /api/products/{id}
+    //retrun 200 or 404
 }
 
